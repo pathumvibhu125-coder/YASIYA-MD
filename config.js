7ac0ec3619456bef368dca6da3766c51dfb15217
pathumvibhu125-coder <pathumvibhu125@gmail.com>
2025-09-27 09:52:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || ''
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD~FNBWSSYR#opk9JVI9l7vWLiToDeWqgJgHOPB1Ugk4nIIo2Upa5as'
 };
